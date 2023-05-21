058a60bc72d90b94bce5fbc44668dccd7fe99410
ritik-mewada <ritikmewada@gmail.com>
2023-05-21 17:57:21 +0530

    product display done

--- client/src/App.js
+++ client/src/App.js
@@ -6,30 +6,29 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
 import Dashboard from "scenes/dashboard";
 import Layout from "scenes/layout";
+import Products from "scenes/products";
 
 function App() {
-    const mode = useSelector((state) => state.global.mode);
-    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
-    return (
-        <div className="app">
-            <BrowserRouter>
-                <ThemeProvider theme={theme}>
-                    <CssBaseline />
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
 
-                    <Routes>
-                        <Route element={<Layout />}>
-                            <Route
-                                path="/"
-                                element={<Navigate to="/dashboard" replace />}
-                            />
-                            <Route path="/dashboard" element={<Dashboard />} />
-                        </Route>
-                    </Routes>
-                </ThemeProvider>
-            </BrowserRouter>
-        </div>
-    );
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ import salesRoutes from "./routes/sales.js";
 
 // import data to db
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
 
 /* configuration */
 dotenv.config();
@@ -44,6 +46,8 @@ mongoose
     );
     // only add data one time
     // User.insertMany(dataUser);
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
   })
   .catch((err) => {
     console.log(`${err} did not connect`);
