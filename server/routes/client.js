fbe6a924fffd9a37b020b52929aef52e14313e79
ritik-mewada <ritikmewada@gmail.com>
2023-05-21 18:41:54 +0530

    customer data display done

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
-import { getProducts } from "../controllers/client.js";
+import { getProducts, getCustomers } from "../controllers/client.js";
 
 router.get("/products", getProducts);
+router.get("/customers", getCustomers);
 
 export default router;
