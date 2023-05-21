c2e82bde3fe0e6be3b73e28dc98c0f8597e727fb
ritik-mewada <ritikmewada@gmail.com>
2023-05-21 15:58:27 +0530

    added user profile to sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Box,
   Divider,
@@ -25,19 +26,78 @@ import {
   CalendarMonthOutlined,
   AdminPanelSettingsOutlined,
   TrendingUpOutlined,
-  PieChartOutline,
   PieChartOutlined,
 } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import FlexBetween from "./FlexBetween";
-import ProfileImage from "assets/profile.jpeg";
+import profileImage from "assets/profile.jpeg";
+
+const navItems = [
+  {
+    text: "Dashboard",
+    icon: <HomeOutlined />,
+  },
+  {
+    text: "Client Facing",
+    icon: null,
+  },
+  {
+    text: "Products",
+    icon: <ShoppingCartOutlined />,
+  },
+  {
+    text: "Customers",
+    icon: <Groups2Outlined />,
+  },
+  {
+    text: "Transactions",
+    icon: <ReceiptLongOutlined />,
+  },
+  {
+    text: "Geography",
+    icon: <PublicOutlined />,
+  },
+  {
+    text: "Sales",
+    icon: null,
+  },
+  {
+    text: "Overview",
+    icon: <PointOfSaleOutlined />,
+  },
+  {
+    text: "Daily",
+    icon: <TodayOutlined />,
+  },
+  {
+    text: "Monthly",
+    icon: <CalendarMonthOutlined />,
+  },
+  {
+    text: "Breakdown",
+    icon: <PieChartOutlined />,
+  },
+  {
+    text: "Management",
+    icon: null,
+  },
+  {
+    text: "Admin",
+    icon: <AdminPanelSettingsOutlined />,
+  },
+  {
+    text: "Performance",
+    icon: <TrendingUpOutlined />,
+  },
+];
 
 const Sidebar = ({
+  user,
   drawerWidth,
-  isNonMobile,
   isSidebarOpen,
   setIsSidebarOpen,
+  isNonMobile,
 }) => {
   const { pathname } = useLocation();
   const [active, setActive] = useState("");
@@ -47,65 +107,6 @@ const Sidebar = ({
   useEffect(() => {
     setActive(pathname.substring(1));
   }, [pathname]);
-
-  const navItems = [
-    {
-      text: "Dashboard",
-      icon: <HomeOutlined />,
-    },
-    {
-      text: "Client Facing",
-      icon: null,
-    },
-    {
-      text: "Products",
-      icon: <ShoppingCartOutlined />,
-    },
-    {
-      text: "Customers",
-      icon: <Groups2Outlined />,
-    },
-    {
-      text: "Transactions",
-      icon: <ReceiptLongOutlined />,
-    },
-    {
-      text: "Geography",
-      icon: <PublicOutlined />,
-    },
-    {
-      text: "Sales",
-      icon: null,
-    },
-    {
-      text: "Overview",
-      icon: <PointOfSaleOutlined />,
-    },
-    {
-      text: "Daily",
-      icon: <TodayOutlined />,
-    },
-    {
-      text: "Monthly",
-      icon: <CalendarMonthOutlined />,
-    },
-    {
-      text: "Breakdown",
-      icon: <PieChartOutlined />,
-    },
-    {
-      text: "Management",
-      icon: null,
-    },
-    {
-      text: "Admin",
-      icon: <AdminPanelSettingsOutlined />,
-    },
-    {
-      text: "Performance",
-      icon: <TrendingUpOutlined />,
-    },
-  ];
 
   return (
     <Box component="nav">
@@ -120,7 +121,7 @@ const Sidebar = ({
             "& .MuiDrawer-paper": {
               color: theme.palette.secondary[200],
               backgroundColor: theme.palette.background.alt,
-              boxSizing: "border-box",
+              boxSixing: "border-box",
               borderWidth: isNonMobile ? 0 : "2px",
               width: drawerWidth,
             },
@@ -150,10 +151,10 @@ const Sidebar = ({
                     </Typography>
                   );
                 }
-
                 const lcText = text.toLowerCase();
+
                 return (
-                  <ListItem key={text} disabledPadding>
+                  <ListItem key={text} disablePadding>
                     <ListItemButton
                       onClick={() => {
                         navigate(`/${lcText}`);
@@ -191,6 +192,42 @@ const Sidebar = ({
               })}
             </List>
           </Box>
+
+          {/* <Box position="absolute" bottom="1rem">
+            <Divider />
+            <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="40px"
+                width="40px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.9rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.8rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <SettingsOutlined
+                sx={{
+                  color: theme.palette.secondary[300],
+                  fontSize: "25px ",
+                }}
+              />
+            </FlexBetween>
+          </Box> */}
         </Drawer>
       )}
     </Box>
