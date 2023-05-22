a91c90402c3677406303bd3d81b39b6d2e70092a
ritik-mewada <ritikmewada@gmail.com>
2023-05-22 12:42:58 +0530

    geography map done

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Layout from "scenes/layout";
 import Products from "scenes/products";
 import Customers from "scenes/customers";
 import Transaction from "scenes/transactions";
+import Geography from "scenes/geography";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -27,6 +28,7 @@ function App() {
               <Route path="/products" element={<Products />} />
               <Route path="/customers" element={<Customers />} />
               <Route path="/transactions" element={<Transaction />} />
+              <Route path="/geography" element={<Geography />} />
             </Route>
           </Routes>
         </ThemeProvider>
--- server/routes/client.js
+++ server/routes/client.js
@@ -4,10 +4,12 @@ import {
   getProducts,
   getCustomers,
   getTransactions,
+  getGeography,
 } from "../controllers/client.js";
 
 router.get("/products", getProducts);
 router.get("/customers", getCustomers);
 router.get("/transactions", getTransactions);
+router.get("/geography", getGeography);
 
 export default router;
